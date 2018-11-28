66e10928ffeaf14658c6f6c44e14120c9b238965
Dave Malone <dmmal@amazon.com>
2018-11-28 21:44:44 +0000

    refining automated cloud9 init script

--- cloud9-init.sh
+++ cloud9-init.sh
@@ -2,6 +2,11 @@
 
 echo Installing NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm --version 
 
 echo Installing Node 
@@ -12,7 +17,7 @@ node --version
 
 echo Installing Yarn
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-sudo yum install yarn
+sudo yum install -y yarn
 yarn --version
 
 echo Installing Amplify CLI
